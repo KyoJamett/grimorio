531a3dbc661db300628a6d77f249f83164bb6bf9
--replace-all <ignacio.jamett.r@gmail.com>
2024-08-13 21:16:40 -0400

    limpieza de codigo

--- src/images/images.jsx
+++
@@ -1,7 +0,0 @@
-const images = {
-  chincol: new URL("./chincol.jpg", import.meta.url).href,
-  diuca: new URL("./diuca.jpg", import.meta.url).href,
-  tenca: new URL("./tenca.jpg", import.meta.url).href,
-};
-
-export default images;
