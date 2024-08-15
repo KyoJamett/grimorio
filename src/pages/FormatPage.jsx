9352184c3e4c9dba82624e93dbea4ed5175c2387
--replace-all <ignacio.jamett.r@gmail.com>
2024-08-15 17:56:50 -0400

    se agregan las ediciones de escuelas

--- src/pages/FormatPage.jsx
+++ src/pages/FormatPage.jsx
@@ -16,35 +16,101 @@ export function FormatPage() {
             <div className="row">
               <p>{format.details}</p>
             </div>
-            <div className="col">
-              Axis Mundi
-              <NavLink
-                className="btn btn-dark"
-                to={"/grimorio/cartas"}
-                state={{ ed: "axis-mundi" }}
-              >
-                Dark
-              </NavLink>
+            <div className="row">
+              <div className="col">
+                Bushido
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "bushido" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
+              <div className="col">
+                Sol Naciente
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "sol-naciente" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
+              <div className="col">
+                Dominio
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "dominio" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
             </div>
-            <div className="col">
-              Hijos del Sol
-              <NavLink
-                className="btn btn-dark"
-                to={"/grimorio/cartas"}
-                state={{ ed: "hijos-del-sol" }}
-              >
-                Dark
-              </NavLink>
+            <div className="row">
+              <div className="col">
+                Contraataque
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "contraataque" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
+              <div className="col">
+                Águila Imperial
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "aguila-imperial" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
+              <div className="col">
+                Steampunk
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "steampunk" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
             </div>
-            <div className="col">
-              Legado Gótico
-              <NavLink
-                className="btn btn-dark"
-                to={"/grimorio/cartas"}
-                state={{ ed: "legado-gotico" }}
-              >
-                Dark
-              </NavLink>
+            <div className="row">
+              <div className="col">
+                Axis Mundi
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "axis-mundi" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
+              <div className="col">
+                Hijos del Sol
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "hijos-del-sol" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
+              <div className="col">
+                Legado Gótico
+                <NavLink
+                  className="btn btn-dark"
+                  to={"/grimorio/cartas"}
+                  state={{ ed: "legado-gotico" }}
+                >
+                  Dark
+                </NavLink>
+              </div>
             </div>
           </div>
         </div>
