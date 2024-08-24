2ef909ab7f9805660e324b5a7dd63c7deff42e9a
--replace-all <ignacio.jamett.r@gmail.com>
2024-08-23 20:45:17 -0400

    cambio de columnas y filas por grid en vista de formato

--- src/pages/FormatPage.jsx
+++ src/pages/FormatPage.jsx
@@ -7,237 +7,198 @@ export function FormatPage() {
   return (
     <>
       <div className="container my-4">
-        <div className="container my-4">
-          <h3>{format.name}</h3>
-          <div className="row">
-            <div className="row">
-              <p>{format.intro}</p>
-            </div>
-            <div className="row">
-              <p>{format.details}</p>
-            </div>
-            <div className="row">
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+        <h3>{format.name}</h3>
+        <p>{format.intro}</p>
+        <p>{format.details}</p>
+        <div className="grid-container">
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/bushido.png"
+                alt="Bushido"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.bu}`}
+                state={{ ed: "bushido" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/bushido.png"
-                    alt="Bushido"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.bu}`}
-                    state={{ ed: "bushido" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/sol_naciente.png"
+                alt="Sol Naciente"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.sn}`}
+                state={{ ed: "sol-naciente" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center ">
-                  <img
-                    src="https://api.myl.cl/static/sol_naciente.png"
-                    alt="Sol Naciente"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.sn}`}
-                    state={{ ed: "sol-naciente" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/dominio.png"
+                alt="Dominio"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.dom}`}
+                state={{ ed: "dominio" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/dominio.png"
-                    alt="Dominio"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.dom}`}
-                    state={{ ed: "dominio" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-            </div>
-            <div className="row">
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/contraataque.png"
+                alt="Contraataque"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.con}`}
+                state={{ ed: "contraataque" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/contraataque.png"
-                    alt="Contraataque"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.con}`}
-                    state={{ ed: "contraataque" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/aguila_imperial.png"
+                alt="Aguila"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.agu}`}
+                state={{ ed: "aguila-imperial" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/aguila_imperial.png"
-                    alt="Aguila"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.agu}`}
-                    state={{ ed: "aguila-imperial" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/steampunk.png"
+                alt="Steampunk"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.stm}`}
+                state={{ ed: "steampunk" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/steampunk.png"
-                    alt="Steampunk"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.stm}`}
-                    state={{ ed: "steampunk" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-            </div>
-            <div className="row">
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/axis-mundi.png"
+                alt="Axis"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.axs}`}
+                state={{ ed: "axis-mundi" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/axis-mundi.png"
-                    alt="Axis"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.axs}`}
-                    state={{ ed: "axis-mundi" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/hijos_del_sol.png"
+                alt="Hijos del Sol"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.sol}`}
+                state={{ ed: "hijos-del-sol" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/hijos_del_sol.png"
-                    alt="Hijos del Sol"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.sol}`}
-                    state={{ ed: "hijos-del-sol" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-              <div
-                className="col d-flex flex-column align-items-center"
-                style={{ maxWidth: "300px" }}
+                Ver
+              </NavLink>
+            </span>
+          </div>
+          <div className="grid-item">
+            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center">
+              <img
+                src="https://api.myl.cl/static/legado-gotico.png"
+                alt="Legado"
+                className="img-fluid"
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  objectFit: "contain",
+                }}
+              />
+              <NavLink
+                className="btn btn-dark mt-2 d-flex justify-content-center"
+                to={`/grimorio/cartas/${escuelas.leg}`}
+                state={{ ed: "legado-gotico" }}
               >
-                <span className="border border-secondary rounded p-3 mb-3 align-items-center">
-                  <img
-                    src="https://api.myl.cl/static/legado-gotico.png"
-                    alt="Legado"
-                    className="img-fluid" // Opcional: Ajusta el tamaño de la imagen si es necesario
-                    style={{
-                      width: "150px",
-                      height: "150px",
-                      objectFit: "contain",
-                    }}
-                  />
-                  <NavLink
-                    className="btn btn-dark mt-2 d-flex justify-content-center"
-                    to={`/grimorio/cartas/${escuelas.leg}`}
-                    state={{ ed: "legado-gotico" }}
-                  >
-                    Ver
-                  </NavLink>
-                </span>
-              </div>
-            </div>
+                Ver
+              </NavLink>
+            </span>
           </div>
         </div>
       </div>
