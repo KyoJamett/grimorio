e2cd492a4b5e94a3fc93bafd4852f3573dafafd6
--replace-all <ignacio.jamett.r@gmail.com>
2024-08-30 15:11:44 -0400

    se agrega cabecera para documentos

--- src/pages/FormatPage.jsx
+++ src/pages/FormatPage.jsx
@@ -44,35 +44,40 @@ export function FormatPage() {
   }
 
   return (
-    <div className="container my-4">
-      <h3>{formato.name}</h3>
-      <p className="justificado">{formato.intro}</p>
-      <p className="justificado">{formato.details}</p>
-      <div className="grid-container">
-        {formato.ediciones.map((element) => (
-          <div className="grid-item" key={element.ed}>
-            <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center black">
-              <img
-                src={`https://api.myl.cl/static/${element.edImg}`}
-                alt={element.ed}
-                className="img-fluid"
-                style={{
-                  width: "150px",
-                  height: "150px",
-                  objectFit: "contain",
-                }}
-              />
-              <NavLink
-                className="btn btn-warning mt-2 d-flex justify-content-center"
-                to={`/grimorio/cartas/${element.ed}`}
-                state={{ ed: element.ed }}
-              >
-                Ver
-              </NavLink>
-            </span>
-          </div>
-        ))}
+    <>
+      <div className="container my-4">
+        <h3>{formato.name}</h3>
+        <p className="justificado">{formato.intro}</p>
+        <p className="justificado">{formato.details}</p>
+        <div className="grid-container">
+          {formato.ediciones.map((element) => (
+            <div className="grid-item" key={element.ed}>
+              <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center black">
+                <img
+                  src={`https://api.myl.cl/static/${element.edImg}`}
+                  alt={element.ed}
+                  className="img-fluid"
+                  style={{
+                    width: "150px",
+                    height: "150px",
+                    objectFit: "contain",
+                  }}
+                />
+                <NavLink
+                  className="btn btn-warning mt-2 d-flex justify-content-center"
+                  to={`/grimorio/cartas/${element.ed}`}
+                  state={{ ed: element.ed }}
+                >
+                  Ver
+                </NavLink>
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="container my-4">
+        <h4>Documentos</h4>
+      </div>
+    </>
   );
 }
