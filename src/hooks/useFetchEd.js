8ccbc76bb44ad01f897ec7f86e793a9b0ec5fdb2
--replace-all <ignacio.jamett.r@gmail.com>
2024-12-04 13:20:16 -0300

    cambio de api paa apuntar a firebase

--- src/hooks/useFetchEd.js
+++ src/hooks/useFetchEd.js
@@ -1,8 +1,21 @@
 import { useEffect, useState } from "react";
 
-export function useFetchEd(ed) {
+/*export function useFetchEd(ed) {
   const [dataCards, setDataCards] = useState(null);
   let url = "https://api.myl.cl/cards/edition/" + ed;
+  useEffect(() => {
+    console.log("Haciendo solicitud a la URL:", url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setDataCards(data));
+  }, []);
+
+  return { dataCards };
+}*/
+export function useFetchEd(ed) {
+  const [dataCards, setDataCards] = useState(null);
+  let url =
+    "https://grimorio-4af23-default-rtdb.firebaseio.com/" + ed + ".json";
   useEffect(() => {
     console.log("Haciendo solicitud a la URL:", url);
     fetch(url)
