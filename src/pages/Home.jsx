8dd22fbd3802ba5b939df6e0b237cf51e65b49b1
Ignacio <kyo.akamatsu@gmail.com>
2025-02-12 16:13:47 -0300

    se extrae grid de cartas a componente CardGrid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useCards } from "../hooks/useCards";
 import ShortcutComponent from "../components/ShortCutComponent";
 import { CardSearch } from "../components/CardSearch";
+import { CardGrid } from "../components/CardGrid";
 
 export function Home() {
   const { intro } = useCards();
@@ -24,49 +25,11 @@ export function Home() {
         <h6 className="cursiva">Sabiduría Infinita</h6>
         <p className="justificado">{intro.text}</p>
         <ShortcutComponent />
-        {/* Formulario de entrada */}
         <br></br>
         <CardSearch setCards={setCards}/>
 
         {/* Grid de cartas, visible solo cuando hay cartas */}
-        <div className="container my-4">
-          {Array.isArray(cards) && cards.length > 0 ? (
-            <>
-              <h4>Resultados de búsqueda</h4>
-              <div className="grid-container">
-                {cards.map((card, index) => (
-                  <div
-                    className="grid-item"
-                    key={`${card.edition}-${card.edid}`}
-                  >
-                    <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center black">
-                      <h6 className="card-title results-title text-warning margin-bottom">
-                        {card.name}
-                      </h6>
-                      <img
-                        src={`https://api.myl.cl/static/cards/${card.edition}/${card.edid}.png`}
-                        alt={card.edid}
-                        className="img-fluid"
-                        style={{
-                          width: "250px",
-                          height: "250px",
-                          objectFit: "contain",
-                          cursor: "pointer",
-                        }}
-                        onClick={() => handleImageClick(card)}
-                      />
-                    </span>
-                  </div>
-                ))}
-              </div>
-            </>
-          ) : Array.isArray(cards) && cards.length == 0 ? (
-            ""
-          ) : (
-            // Si cards no es un array o está vacío
-            <p>No se encontraron resultados.</p>
-          )}
-        </div>
+        <CardGrid cards={cards} handleImageClick={handleImageClick}/>
 
         {/* Modal para mostrar imagen expandida */}
         {selectedImage && (
