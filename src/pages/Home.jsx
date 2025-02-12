5f804769dad58d703c6715a412558a57a4cca3a5
Ignacio <kyo.akamatsu@gmail.com>
2025-02-12 16:30:40 -0300

    se extrae modal de busqueda a componente CardSearchModal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useCards } from "../hooks/useCards";
 import ShortcutComponent from "../components/ShortCutComponent";
 import { CardSearch } from "../components/CardSearch";
 import { CardGrid } from "../components/CardGrid";
+import { CardSearchModal } from "../components/CardSearchModal";
 
 export function Home() {
   const { intro } = useCards();
@@ -26,46 +27,14 @@ export function Home() {
         <p className="justificado">{intro.text}</p>
         <ShortcutComponent />
         <br></br>
+        {/* Barra de busqueda */}
         <CardSearch setCards={setCards}/>
 
         {/* Grid de cartas, visible solo cuando hay cartas */}
         <CardGrid cards={cards} handleImageClick={handleImageClick}/>
 
         {/* Modal para mostrar imagen expandida */}
-        {selectedImage && (
-          <div
-            className="modal show d-block abrir-modal animacion fadeIn"
-            style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
-            onClick={handleCloseModal}
-          >
-            <div className="modal-dialog modal-lg modal-dialog-centered">
-              <div
-                className="modal-content"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <div className="modal-header">
-                  <h5 className="modal-title">{selectedImage.name}</h5>
-                  <button
-                    type="button"
-                    className="btn-close"
-                    onClick={handleCloseModal}
-                  />
-                </div>
-                <div className="modal-body text-center">
-                  <img
-                    src={`https://api.myl.cl/static/cards/${selectedImage.edition}/${selectedImage.edid}.png`}
-                    alt={selectedImage.edid}
-                    className="img-fluid"
-                    style={{
-                      maxHeight: "80vh",
-                      width: "auto",
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
+        <CardSearchModal handleCloseModal={handleCloseModal} selectedImage={selectedImage}/>
       </div>
     </>
   );
