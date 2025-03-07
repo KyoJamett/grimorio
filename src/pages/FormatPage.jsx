36a140fa0001c6bb70acac02076a665efca41558
Ignacio <kyo.akamatsu@gmail.com>
2025-03-07 16:34:36 -0300

    feat: crear componente EditionGrid para mostrar ediciones de un formato

--- src/pages/FormatPage.jsx
+++ src/pages/FormatPage.jsx
@@ -1,6 +1,7 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useCards } from "../hooks/useCards";
 import { UseFormats } from "../hooks/useFormats";
+import { EditionGrid } from "../components/EditionGrid";
 
 export function FormatPage() {
   // const { furia, escuelas, civilizaciones, expediciones, pb, pe } =
@@ -45,31 +46,7 @@ export function FormatPage() {
         <p className="justificado">{formato.details}</p>
         {/* Fin componentes de info del formato */}
         {/* --------------------------------------------------Componente grid de ediciones*/ }
-        <div className="grid-container">
-          {formato.ediciones.map((element) => (
-            <div className="grid-item" key={element.ed}>
-              <span className="border border-secondary rounded p-3 mb-3 d-flex flex-column align-items-center black">
-                <img
-                  src={`${element.edImg}`}
-                  alt={element.ed}
-                  className="img-fluid"
-                  style={{
-                    width: "150px",
-                    height: "150px",
-                    objectFit: "contain",
-                  }}
-                />
-                <NavLink
-                  className="btn btn-warning mt-2 d-flex justify-content-center"
-                  to={`/cartas/${element.ed}`}
-                  state={{ ed: element.ed }}
-                >
-                  Ver
-                </NavLink>
-              </span>
-            </div>
-          ))}
-        </div>
+        <EditionGrid ediciones={formato.ediciones}/>
         {/* Fin componente grid de ediciones*/ }
       </div>
       <div className="container my-4">
