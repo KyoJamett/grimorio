c65c26fe4e8cc66b6debbd498206a00f955e6f85
Ignacio <kyo.akamatsu@gmail.com>
2025-06-04 12:09:17 -0400

    feat: se crea componente Footer

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { CardRoutes } from "./routes/CardRoutes";
+import { Footer } from "./components/layout/Footer";
+import { Navigationbar } from "./components/layout/Navigationbar";
 
 export const MainApp = () => {
   return (
-    <div className="app-background pb-4">
-      <Routes>
-        <Route path="/*" element={<CardRoutes />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app-background pb-4">
+        <Routes>
+          <Route path="/*" element={<CardRoutes />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-light py-5">
+      <div className="container px-4 px-lg-5">
+        <div className="small text-center text-muted">
+          Copyright &copy; 2025 - Ignacio Jamett
+        </div>
+      </div>
+    </footer>
+  );
+};
