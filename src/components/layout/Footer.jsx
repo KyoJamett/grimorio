ca9de8e0a6ab55e397c0b4b14878eb27de45435b
Ignacio <kyo.akamatsu@gmail.com>
2025-06-04 14:50:01 -0400

    feat: se cambian estilos de footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,8 @@
 export const Footer = () => {
   return (
-    <footer className="bg-light py-5">
+    <footer className="bg-dark py-5 font-footer">
       <div className="container px-4 px-lg-5">
-        <div className="small text-center text-muted">
+        <div className="small text-center">
           Copyright &copy; 2025 - Ignacio Jamett
         </div>
       </div>
