9ed5c11b6b1b9db464f6547c6cc65bd3f11a39ac
KyoJamett <kyo.akamatsu@gmail.com>
2026-03-26 14:30:55 -0300

    refactor: replace direct Firebase calls with backend API in useFetchEd

--- src/hooks/useFetchEd.js
+++ src/hooks/useFetchEd.js
@@ -1,27 +1,14 @@
 import { useEffect, useState } from "react";
 
-/*export function useFetchEd(ed) {
-  const [dataCards, setDataCards] = useState(null);
-  let url = "https://api.myl.cl/cards/edition/" + ed;
-  useEffect(() => {
-    console.log("Haciendo solicitud a la URL:", url);
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setDataCards(data));
-  }, []);
-
-  return { dataCards };
-}*/
 export function useFetchEd(ed) {
   const [dataCards, setDataCards] = useState(null);
-  let url =
-    "https://grimorio-4af23-default-rtdb.firebaseio.com/" + ed + ".json";
+  let url = `http://localhost:3001/api/edition/${ed}`;
   useEffect(() => {
     console.log("Haciendo solicitud a la URL:", url);
     fetch(url)
       .then((response) => response.json())
       .then((data) => setDataCards(data));
-  }, []);
+  }, [ed]);
 
   return { dataCards };
 }
