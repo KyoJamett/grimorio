1b09a4d66c890629fd0cb247bfee1069766642e0
KyoJamett <kyo.akamatsu@gmail.com>
2026-03-30 16:57:26 -0300

    feat: footer styles fixed

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-dark py-5 font-footer">
+    <footer className="bg-dark py-3 font-footer">
       <div className="container px-4 px-lg-5">
         <div className="small text-center">
           Copyright &copy; 2025 - Ignacio Jamett
