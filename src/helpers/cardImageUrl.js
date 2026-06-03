c82b368e9dee69363bf402dca4689be2d102a5f1
KyoJamett <kyo.akamatsu@gmail.com>
2026-06-03 18:53:24 -0400

    fix: env variables fix

--- src/helpers/cardImageUrl.js
+++ src/helpers/cardImageUrl.js
@@ -1,5 +1,5 @@
 export function getCardImageUrl(ed_edid, edid) {
   const safeEd = ed_edid || '00';
   const safeEdid = edid || '000';
-  return `${import.meta.env.VITE_API_URL}/api/cards/${safeEd}/${safeEdid}`;
+  return `${import.meta.env.VITE_API_IMAGES}/${safeEd}/${safeEdid}.${import.meta.env.VITE_IMG_EXTENSION}`;
 }
