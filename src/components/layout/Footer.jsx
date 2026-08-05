2efa9c964ccfce6806e5e208a5e8acdebbcd89b7
KyoJamett <kyo.akamatsu@gmail.com>
2026-08-04 21:33:47 -0400

    feat: redesign disclaimer footer link

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,8 +3,11 @@ import { NavLink } from "react-router-dom";
 export const Footer = () => {
   return (
     <footer className="fixed-bottom bg-dark py-2 font-footer">
-      <div className="container px-4 px-lg-5">
-        <NavLink className="small text-center" to={`/disclaimer`}>
+      <div className="w-100 px-3">
+        <NavLink
+          className="small link-light link-offset-2 link-underline link-underline-opacity-0"
+          to={`/disclaimer`}
+        >
           🛈 Descargo de responsabilidad
         </NavLink>
         <div></div>
